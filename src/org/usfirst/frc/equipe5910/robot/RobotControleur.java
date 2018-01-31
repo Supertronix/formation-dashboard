c5d18a4014e4bcd2cf8df6c32fc69235812837fd
nadineducegep <nadineducegep@gmail.com>
2018-01-31 10:33:06 -0500

    Affichages dans le dashboard : texte du mode, compteur de tour, sélecteur de mode autonome

--- src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
+++ src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.equipe5910.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,6 +14,16 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class RobotControleur extends IterativeRobot {
 
+	public class CommandeImmobile extends CommandGroup { }
+	public class CommandeTrajetR1 extends CommandGroup { }
+	public class CommandeTrajetR2 extends CommandGroup { }
+	public class CommandeTrajetR3 extends CommandGroup { }
+	public class CommandeTrajetB1 extends CommandGroup { }
+	public class CommandeTrajetB2 extends CommandGroup { }
+	public class CommandeTrajetB3 extends CommandGroup { }
+	
+    protected SendableChooser<CommandGroup> selecteur;
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -19,6 +32,16 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		System.out.println("robotInit()");
+		// http://wpilib.screenstepslive.com/s/3120/m/7932/l/81109-choosing-an-autonomous-program-from-smartdashboard
+		selecteur = new SendableChooser<CommandGroup>();
+		selecteur.addDefault("No move", new CommandeImmobile());
+		selecteur.addObject("R1", new CommandeTrajetR1());
+		selecteur.addObject("R2", new CommandeTrajetR2());
+		selecteur.addObject("R3", new CommandeTrajetR3());	
+		selecteur.addObject("B1", new CommandeTrajetB1());
+		selecteur.addObject("B2", new CommandeTrajetB2());
+		selecteur.addObject("B3", new CommandeTrajetB3());	
+		SmartDashboard.putData("Autonomous mode chooser", selecteur);
 	}
 
 	/**
@@ -27,14 +50,18 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void autonomousInit() {
 		System.out.println("autonomousInit()");
+		SmartDashboard.putString("Mode", "Autonome");		
 	}
 
+	int periodeAutonome = 0;
+	
 	/**
 	 * This function is called periodically during autonomous
 	 */
 	@Override
 	public void autonomousPeriodic() {
 		System.out.println("autonomousPeriodic()");
+		SmartDashboard.putNumber("Periodique", periodeAutonome++);
 	}
 
 	/**
@@ -44,14 +71,18 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void teleopInit() {
 		System.out.println("teleopInit()");
+		SmartDashboard.putString("Mode", "Teleop");
 	}
 
+	int periodeTeleop = 0;
+	
 	/**
 	 * This function is called periodically during operator control
 	 */
 	@Override
 	public void teleopPeriodic() {
 		System.out.println("teleopPeriodic()");
+		SmartDashboard.putNumber("Periodique", periodeTeleop++);
 	}
 
 	/**
